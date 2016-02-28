f7f1d9b3589742ccda6622f8abf89c374a2e7410
KatyaBondarenko <superKatya25>
2016-02-29 01:27:49 +0200

    function areInterseсted

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
 ﻿
+
+function areInterseсted (firstRec , secondRec){
+
+
+    var firstRec = {
+        top: undefined,
+        left: undefined,
+        width: undefined,
+        height: undefined
+    }
+
+    var secondRec = {
+        top: undefined,
+        left: undefined,
+        width: undefined,
+        height: undefined
+    }
+
+
+    var firstBottom,
+        firstRight,
+        secondBottom,
+        secondRight
+        result = true;
+
+
+    firstBottom = firstRec.top + firstRec.width + firstRec.height;
+    firstRight = firstRec.left + firstRec.height + firstRec.width;
+    secondBottom = secondRec.top + secondRec.width + secondRec.height;
+    secondRight = secondRec.left + secondRec.height + secondRec.width;
+
+
+
+    if (firstRec.left < secondRight || firstRight > secondRec.left || firstBottom < secondRec.top || firstRec.top > secondBottom) {
+
+
+
+        result = false;
+
+    }
+
+    return result;
+
+}
+
+
+areInterseсted({
+    left: 0,
+    top: 0,
+    width: 20,
+    height: 20
+}, {
+    left: 10,
+    top: 10,
+    width: 25,
+    height: 25
+});
+
+console.log(result);
+
+    function filterVisible (){
+
+
+
+    }
